3f8904b390ee6fd38aa58f811d4cf6179b993eb8
miyucy <fistfvck@gmail.com>
2016-10-20 02:06:28 +0900

    .

--- skyway.js
+++ skyway.js
@@ -1,7 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
   navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(function (mediaStream) {
-    var flags = { video: true, audio: false };
+    var flags = { video: false, audio: false };
     var peer = new Peer({ key: 'b940b211-b187-4c52-9982-7ab24a648b20' });
+
+    updateButtons(flags);
+    for (var track of mediaStream.getTracks()) {
+      track.enabled = false;
+    }
 
     peer.on('open', function (peerId) {
       var node = document.getElementById('localpeerid');
@@ -27,7 +32,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     document.getElementById('video').addEventListener('click', function (evt) {
       flags.video = !flags.video;
-      evt.currentTarget.textContent = 'video ' + (flags.video ? 'off' : 'on');
+      updateButtons(flags);
       for (var track of mediaStream.getVideoTracks()) {
         track.enabled = flags.video;
       }
@@ -35,13 +40,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
     document.getElementById('audio').addEventListener('click', function (evt) {
       flags.audio = !flags.audio;
-      evt.currentTarget.textContent = 'audio ' + (flags.audio ? 'off' : 'on');
+      updateButtons(flags);
       for (var track of mediaStream.getAudioTracks()) {
         track.enabled = flags.audio;
       }
     });
   }).catch(console.error);
 });
+
+function updateButtons(flags) {
+  document.getElementById('video').textContent = 'video ' + (flags.video ? 'off' : 'on');
+  document.getElementById('audio').textContent = 'audio ' + (flags.audio ? 'off' : 'on');
+}
 
 function play(node, mediaStream) {
   node.srcObject = mediaStream;
