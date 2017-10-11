729f0f92b906c06987f2c02a6d83bb30e49385b4
miyucy <fistfvck@gmail.com>
2017-10-11 10:39:49 +0900

    .

---
+++ async.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('DOMContentLoaded:' + +(new Date()));
+});
+
+window.addEventListener('load', function () {
+  console.log('load:' + +(new Date()));
+});
